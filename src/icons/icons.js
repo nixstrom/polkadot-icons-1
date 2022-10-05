37c8158a21fd379db799a4b8b7c5c9e602b41b61
Nikki Strømsnes <hello@tinybits.dk>
2022-10-05 16:33:03 +0200

    Make working POC

---
+++ src/icons/icons.js
@@ -0,0 +1,17 @@
+export const icons = [
+	{
+		name: 'Clock',
+		svg: (color = '#fff', strokeWidth = '2', size = '24') =>
+			`<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" viewBox="0 0 24 24" fill="none" stroke="${color}" stroke-width="${strokeWidth}" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>`,
+	},
+	{
+		name: 'Heart',
+		svg: (color = '#fff', strokeWidth = '2', size = '24') =>
+			`<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" viewBox="0 0 24 24" fill="none" stroke="${color}" stroke-width="${strokeWidth}" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>`,
+	},
+	{
+		name: 'Video',
+		svg: (color = '#fff', strokeWidth = '2', size = '24') =>
+			`<svg xmlns="http://www.w3.org/2000/svg" width="${size}" height="${size}" viewBox="0 0 24 24" fill="none" stroke="${color}" stroke-width="${strokeWidth}" stroke-linecap="round" stroke-linejoin="round" class="feather feather-video"><polygon points="23 7 16 12 23 17 23 7"></polygon><rect x="1" y="5" width="15" height="14" rx="2" ry="2"></rect></svg>`,
+	},
+]
